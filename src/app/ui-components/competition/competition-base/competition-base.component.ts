b73cb803b68ec62a739fc408222e365d6d58e35e
BarnabasLorincz <barnipro@gmail.com>
2018-12-01 13:04:53 +0100

    competition create in progress

---
+++ src/app/ui-components/competition/competition-base/competition-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-base',
+  templateUrl: './competition-base.component.html',
+  styleUrls: ['./competition-base.component.css']
+})
+export class CompetitionBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
