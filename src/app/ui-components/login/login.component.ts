9dc5a1f220eb1cb752ddd8bd7832a389215acac2
toto9702 <44481595+toto9702@users.noreply.github.com>
2018-12-12 15:53:16 +0100

    login

--- src/app/ui-components/login/login.component.ts
+++ src/app/ui-components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {FormBuilder, FormControl, FormControlName, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  message: string;
+  hidePassword = true;
+
+  form: FormGroup;
+  username: string;
+  password: string;
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async onSubmit() {
+    try {
+      this.message = null;
+      await this.authService.login(this.username, this.password);
+      if (this.authService.redirectUrl) {
+        this.router.navigate([this.authService.redirectUrl]);
+      } else {
+        this.router.navigate(['/']);
+      }
+    } catch (e) {
+      this.message = 'Cannot log in!';
+    }
   }
 
 }
